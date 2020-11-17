7a65ab9a6106f1636e41753f757917ea9f24c0fe
haitung1999 <haitung1999@gmail.com>
2020-11-17 18:32:52 +0700

    add splashscreen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import RootRouter from './routers';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import redux from './redux';
 import { LogBox } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 LogBox.ignoreAllLogs()
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={redux.store}>
       <PersistGate loading={null} persistor={redux.persistor}>
--- src/routers/AuthStack.js
+++ src/routers/AuthStack.js
@@ -3,13 +3,11 @@ import { createStackNavigator } from '@react-navigation/stack';
 import SignInScreen from '../screens/SignIn/index';
 import SignUpScreen from '../screens/SignUp/index';
 import AuthScreen from '../screens/user/AuthScreen';
-import SplashScreen from '../screens/Splash';
 
 // Create AuthStack
 const AuthStack = createStackNavigator();
 const authStack = () => (
   <AuthStack.Navigator>
-    <AuthStack.Screen name="Splash" component={SplashScreen} options={{ headerShown: null }} />
     <AuthStack.Screen name="Authentication" component={AuthScreen} options={() => {
       return {
         headerTitle: 'Authenticate',
